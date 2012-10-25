e117b5827a6cc7fc7f4afef8eccb949bdd9ccdbd
Jeremy Green <jeremy@greenhousephotogallery.com>
2012-10-24 23:25:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyxdmRailsExample::Application.config.secret_token = '995449bcd17384e58b7449145e479faf8f60f1a7713d35fcce8c1faa0c747287d777bf3246a3ac900e6ad086ec32f07fbc5ca15b0f60e941cc343161db6352b0'
